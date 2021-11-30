4189959fb65c7bd5d4b3ca08e5553bd7d677a593
GeertKock <g.kock1@student.avans.nl>
2021-11-30 12:12:41 +0100

    Added Fragments working

---
+++ app/src/main/java/com/example/hueapp/Fragment1.kt
@@ -0,0 +1,5 @@
+package com.example.hueapp
+import androidx.fragment.app.Fragment
+
+class Fragment1 : Fragment(R.layout.fragment1) {
+}
--- app/src/main/java/com/example/hueapp/MainActivity.kt
+++ app/src/main/java/com/example/hueapp/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.hueapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.hueapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.Btn1.setOnClickListener{
+
+            replaceFragment(Fragment1())
+
+        }
+
+        binding.Btn2.setOnClickListener{
+
+            replaceFragment(Fragment2())
+
+        }
+
+
+    }
+
+    private fun replaceFragment(fragment : Fragment) {
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer,fragment)
+        fragmentTransaction.commit()
     }
 }
