b1d1480970aee7bb424b507cfebc34a217e0e08d
ddonk <daanvd1000@gmail.com>
2021-11-30 14:28:28 +0100

    [Add] SharedPreferences saves

--- app/src/main/java/com/example/hueapp/MainActivity.kt
+++ app/src/main/java/com/example/hueapp/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.hueapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.SharedPreferences
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val settings = getSharedPreferences("UserInfo", 0)
+        val editor = settings.edit()
+
+        val text = findViewById<View>(R.id.testText) as EditText
+        val testText = findViewById<TextView>(R.id.testTextView)
+
+        val button: Button = findViewById(R.id.testButton)
+        button.setOnClickListener {
+            Log.d("TEST", text.getText().toString())
+            editor.putString("Username", text.getText().toString())
+            editor.commit()
+        }
+
+
+
+        testText.setText(settings.getString("Username", "").toString())
     }
 }
