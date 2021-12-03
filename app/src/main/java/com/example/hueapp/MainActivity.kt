80d1c7657093e09003bb912ed1d1755ecdcc6590
delegateDonk <daanvd1000@gmail.com>
2021-12-03 14:39:36 +0100

    Revert "Merge Save User Settings into dev"

--- app/src/main/java/com/example/hueapp/MainActivity.kt
+++ app/src/main/java/com/example/hueapp/MainActivity.kt
@@ -2,12 +2,6 @@ package com.example.hueapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.content.SharedPreferences
-import android.util.Log
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -46,8 +40,7 @@ class MainActivity : AppCompatActivity() {
         fragmentTransaction.replace(R.id.fragmentContainer,fragment)
         fragmentTransaction.commit()
         setContentView(R.layout.activity_main)
-// User saves
-        val sharedPreferencesManager = SharedPreferencesManager(getSharedPreferences("UserInfo", 0))
+
         
     // START JSON
 //        onCreate(lamp)
